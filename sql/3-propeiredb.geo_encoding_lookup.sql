a6b2342df23477dc6fbd5f581a8b42c59641140a
emmett <emmettcarey95@gmail.com>
2024-06-03 20:59:11 -0700

    got basic function going

--- sql/3-propeiredb.geo_encoding_lookup.sql
+++ sql/3-propeiredb.geo_encoding_lookup.sql
@@ -6,4 +6,17 @@ CREATE TABLE IF NOT EXISTS "propeiredb".geo_encoding_lookup (
     lat DECIMAL(9, 6),
     lon DECIMAL(9, 6),
     region TEXT
-)
+);
+
+
+CREATE OR REPLACE view propeiredb.missing_geo_encoded_addresses as
+select 
+    address_hash,
+    address,
+    sale_date,
+    county,
+    province,
+    postal_code
+from propeiredb.residential_register
+inner join propeiredb.geo_encoding_lookup on propeiredb.residential_register.address_hash = propeiredb.geo_encoding_lookup.address_hash
+where lat is null or lon is null;
